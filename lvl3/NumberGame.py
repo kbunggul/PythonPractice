d344375c295913c333fea6f084088baf9b647164
kbunggul <63137287+kbunggul@users.noreply.github.com>
2020-05-01 13:01:12 +0900

    clearNumberGame

--- lvl3/NumberGame.py
+++ lvl3/NumberGame.py
@@ -1,5 +1,20 @@
 def solution(A,B):
     answer = 0
+    A.sort()
+    B.sort()
+    a = A.pop()
+    b = B.pop()
+    leng = len(A)
+    for i in range(leng):
+        if a >= b:
+            a = A.pop()
+            B.pop(0)
+        else:
+            b = B.pop()
+            a = A.pop()
+            answer += 1       
+    if b > a:
+        answer += 1
     return answer
 
 print(solution([5,1,3,7],[2,2,6,8]))
