ff242360a3f920d09725343151cf99bb34d4737e
kbunggul <63137287+kbunggul@users.noreply.github.com>
2020-05-01 12:50:31 +0900

    Clear

--- lvl3/BaseStation.py
+++ lvl3/BaseStation.py
@@ -1,21 +1,27 @@
 def solution(n,stations, w):
     answer = 0
-    stations.sort()
-    arrival = 0
+    arrival = 1
+    covered = 2 * w + 1
     
     for station in stations:
-        if station - w > arrival:
-            answer += (station - w -arrival) // (2 * w + 1) 
-            if (station - w -arrival) % (2 * w + 1) !=0:
+        if (station - w) > arrival:
+            answer += (station - w - arrival) // covered
+            if (station - w - arrival) % covered != 0:
                 answer += 1
-            arrival = station + w
-    if arrival < n:
-        answer += (station - w -arrival) // (2 * w + 1) 
-        if (station - w -arrival) % (2 * w + 1) !=0:
+        arrival = station + w +1
+    arrival -= 1
+    if arrival <= n:
+        answer += (n - arrival) // covered 
+        if (n-arrival) % covered != 0:
             answer += 1
-        arrival = station + w
         
-    
     return answer
 
-print(solution(11,[4,11],1))
+print(solution(5,[5],1))
+print(solution(6,[6],1))
+print(solution(5,[1],1))
+print(solution(6,[1],1))
+print(solution(14,[4, 14],1))
+print(solution(16,[9],2))
+print(solution(17,[9],2))
+print(solution(9,[2, 7],2))
